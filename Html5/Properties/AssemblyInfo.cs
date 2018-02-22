b18bdd025fa6fa8f92b676ad8ec24a57442f123a
Fabrício Zimmerer Murta <fabricio@object.net>
2018-02-22 16:42:05 -0300

    Updates Html5 project to re-enable building the NuGet package.
    The csproj build directive to trigger package building differs from the
    main Bridge project.

--- Html5/Properties/AssemblyInfo.cs
+++ Html5/Properties/AssemblyInfo.cs
@@ -10,4 +10,10 @@ using Bridge;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("d5956119-f924-47e1-96c2-4d98b832eb38")]
+[assembly: AssemblyVersion("17.0.0")]
+[assembly: AssemblyFileVersion("17.0.0")]
+[assembly: AssemblyInformationalVersion("17.0.0")]
+
+
+// TODO: Remove this and review the changes it makes, so that code still works.
 [assembly: Convention(Member = ConventionMember.Field | ConventionMember.Method, Notation = Notation.LowerCamelCase)]
