832527e2ae2ba6fa020e54cbf26a036f7dbc9423
Geoffrey McGill <geoff@object.net>
2018-02-22 21:30:26 -0700

    [#3431] Move [Ready] Attribute from Html5 to Bridge

--- Html5/Events/EventAttributes.cs
+++ Html5/Events/EventAttributes.cs
@@ -34,23 +34,6 @@ namespace Bridge.Html5
         }
     }
 
-    /// <summary>
-    /// Makes the method to be called once the page is loaded. If using jQuery2, triggers jQuery's event,
-    /// otherwise, uses DOMContentReady event from HTML5.
-    /// </summary>
-    [External]
-    public class ReadyAttribute : EventAttribute
-    {
-        new public const string Format = "Bridge.ready(this.{2});";
-        new public const string FormatScope = "Bridge.ready(this.{2}, this);";
-        public const string Event = "ready";
-        public const bool StaticOnly = true;
-
-        public ReadyAttribute()
-        {
-        }
-    }
-
     [External]
     public class ClickAttribute : EventAttribute
     {
